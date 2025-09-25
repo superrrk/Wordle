7f4d185ef78e255c6cf8ff5404aa3a2b3f3d970e
superrrk <89561578+superrrk@users.noreply.github.com>
2025-09-24 22:37:36 -0400

    Update README.md

--- Wordle/BoardController.swift
+++ Wordle/BoardController.swift
@@ -45,7 +45,15 @@ class BoardController: NSObject,
       // Tip: Use the transform property of the cell. Use transform.scaledBy to modify the scale. This should feel familiar to lab 1.
       // Checkpoint: After finishing this exercise, you should now be able to see that the board animates whenever you enter a new letter! If it's not animating, check your work on this exercise.
       // START YOUR CODE HERE
-      // ...
+        UIView.animate(withDuration: 0.1,
+                       delay: 0.0,
+                       options: [.autoreverse],
+                       animations: {
+            // Exercise 7: bump the scale by 1.05
+            cell.transform = cell.transform.scaledBy(x: 1.05, y: 1.05)
+        }, completion: { finished in
+            cell.transform = CGAffineTransform.identity
+        })
       // END YOUR CODE HERE
     }, completion: { finished in
       cell.transform = CGAffineTransformIdentity
@@ -64,7 +72,8 @@ class BoardController: NSObject,
     // Tip: Checkout the public methods on LetterCell.swift
     // Checkpoint: After finishing this exercise, you should now be able to tap on the delete keyboard cell and have the last letter deleted on the board! If it's not working, check your work on this exercise and make sure deleteLastCharacter() is called properly in exercise 3.
     // START YOUR CODE HERE
-    // ...
+    cell.set(letter: "")
+      cell.set(style: .initial)
     // END YOUR CODE HERE
   }
 }
--- Wordle/ViewController.swift
+++ Wordle/ViewController.swift
@@ -30,7 +30,14 @@ class ViewController: UIViewController {
       Else, it should use the `enter` method of `boardController` and pass in the selected string as the argument.
      */
     // START YOUR CODE HERE
-    // ...
+      keyboardController.didSelectString = { [weak self] key in
+          guard let self = self else { return }
+          if key == DELETE_KEY {
+              self.boardController.deleteLastCharacter()
+          } else {
+              self.boardController.enter(key)
+          }
+      }
     // END YOUR CODE HERE
   }
 }
